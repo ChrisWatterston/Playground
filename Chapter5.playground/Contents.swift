3f51a7a05a1c32c39f925c2629b7ff7fedd878b3
Chris Watterston <hello@chriswatterston.com>
2017-02-27 17:55:34 +0000

    - continued through chapter 5 - starting initialiser section next

--- Chapter5.playground/Contents.swift
+++ Chapter5.playground/Contents.swift
@@ -1,5 +1,101 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
-var str = "Hello, playground"
+// ------------ playing with structure -------------- //
+struct EmployeeStruct {
+    var firstName = ""
+    var lastName = ""
+    
+    var salaryYear: Double = 0.0 {
+        willSet {
+            print("About to set salaryYear to \(newValue)")
+        }
+        didSet {
+            if salaryWeek > oldValue {
+                print("\(firstName) got a raise")
+            } else  {
+                print("\(firstName) did not get a raise")
+            }
+        }
+    }
+    
+    var salaryWeek: Double {
+        get {
+            return self.salaryYear / 52
+        }
+        set {
+            self.salaryYear = newValue * 52
+        }
+    }
+
+    func getFullName() -> String {
+        return firstName + " " + lastName
+    }
+    
+    mutating func giveRaise(amount: Double) {
+        self.salaryYear += amount
+    }
+
+}
+
+var employeeS = EmployeeStruct(firstName: "Ben", lastName: "Dover", salaryYear: 12000)
+print(employeeS.salaryWeek)
+
+employeeS.salaryWeek = 1000
+
+print(employeeS.salaryWeek)
+print(employeeS.salaryYear)
+
+
+// ------------ playing with classes -------------- //
+public class EmployeeClass {
+    var firstName = ""
+    var lastName = ""
+
+    var salaryYear: Double = 0.0 {
+        willSet (newSalary) {
+            print("About to set salaryYear to \(newSalary)")
+        }
+        didSet {
+            if salaryWeek > oldValue {
+                print("\(firstName) got a raise")
+            } else {
+                print("\(firstName) did not get a raise")
+            }
+        }
+    }
+    
+    var salaryWeek: Double {
+        get {
+            return self.salaryYear / 52
+        }
+        set (newSalaryWeek) {
+            self.salaryYear = newSalaryWeek * 52
+        }
+    }
+
+    func getFullName() -> String {
+        return firstName + " " + lastName
+    }
+    
+    func giveRaise(amount: Double) {
+        self.salaryYear += amount
+    }
+    
+}
+
+
+var employeeCl = EmployeeClass()
+var employeeC = EmployeeStruct(firstName: "Ballin", lastName: "Dep", salaryYear: 60000)
+
+employeeCl.firstName = "John"
+employeeCl.lastName = "Digin"
+employeeCl.salaryYear = 50000
+
+employeeCl.giveRaise(amount: 75000)
+employeeC.giveRaise(amount: 1100)
+
+print(employeeCl.getFullName())
+print(employeeC.getFullName())
+
+print(employeeC.salaryYear)
+
